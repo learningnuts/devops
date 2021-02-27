0cde266347e6ca23f4b7bb1fc43052802ce8b005
Vikas Verma <vermavikas1002@gmail.com>
2021-02-27 20:37:04 +0530

    thired

--- devops.java
+++ devops.java
@@ -1,6 +1,6 @@
 public class devops{
 	public static void main(String[] args){
-		for (int i=1; i<=11; i++){
+		for (int i=1; i<=13; i++){
 			System.out.println("DevOps is hot topic these days "+i);
 		}
 	}
